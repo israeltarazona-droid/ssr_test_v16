e415d3208f27f1b9d11ab70f8c9d66c0c9f2f02c
ISRAEL-TARAZONA\israel.tarazona_prag <itarazon@bancolombia.com.co>
2025-08-28 14:19:03 -0500

    update

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,7 @@ import { getAnalytics } from "firebase/analytics";
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
 
-
-  // Import the functions you need from the SDKs you need
+// Import the functions you need from the SDKs you need
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -17,13 +16,13 @@ platformBrowserDynamic().bootstrapModule(AppModule)
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBtmrpT-i6g9bL5Dl1roB5p9y6IHnYUO8I",
-  authDomain: "ssr-test-v16.firebaseapp.com",
-  projectId: "ssr-test-v16",
-  storageBucket: "ssr-test-v16.firebasestorage.app",
-  messagingSenderId: "481725164060",
-  appId: "1:481725164060:web:3c7c05cae8359be6378dd9",
-  measurementId: "G-M49LRNEGKZ"
+  apiKey: "AIzaSyAlYIvHo5RK8x4CHCLABmm-27rboPaX-DY",
+  authDomain: "ssr-test-v16-48d3a.firebaseapp.com",
+  projectId: "ssr-test-v16-48d3a",
+  storageBucket: "ssr-test-v16-48d3a.firebasestorage.app",
+  messagingSenderId: "915151888101",
+  appId: "1:915151888101:web:617087f1c5aaa632c67e0c",
+  measurementId: "G-Z86SQF9YQ5"
 };
 
 // Initialize Firebase
